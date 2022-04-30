6040ec718cac5b19f92e2a0f8447b864138eef29
Vincent Nguyen <vincentbanguyen@gmail.com>
2022-04-30 15:19:06 -0500

    Initial Commit

---
+++ Shared/CheerioApp.swift
@@ -0,0 +1,17 @@
+//
+//  CheerioApp.swift
+//  Shared
+//
+//  Created by Vincent Nguyen on 4/30/22.
+//
+
+import SwiftUI
+
+@main
+struct CheerioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
